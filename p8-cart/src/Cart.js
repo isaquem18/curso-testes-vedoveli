e8f4ba8cf8da32b3bfcfc585f0296bfc1896284d
Isaque Maninde Moura Patricio <isaquem18@gmail.com>
2022-05-19 12:02:07 -0300

    teste

--- p8-cart/src/Cart.js
+++ p8-cart/src/Cart.js
@@ -1,23 +1,57 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+
 export default class Cart {
   items = [];
   
   // calcular o total a ser pago considerando também condições de descontos
   getTotal() {
     return this.items.reduce((acc, item) => {
-      return acc += item.quantity * item.product.price;
+
+      if (item.condition?.percentage) {
+
+        return acc += item.product.price * item.quantity;
+      }
+
+      return acc += item.product.price * item.quantity;
     }, 0);
   };
 
   add(item) {
+    const itemToFind = { product: item.product };
+
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind)
+    };
+
     this.items.push(item);
   };
 
-  remove(product) {};
+  remove(product) {
+    remove(this.items, { product })
+  };
 
   // retorna tudo que se tem no cart
-  summary() {};
+  summary() {
+    const total = this.getTotal();
+    const items = this.items;
+
+    return {
+      total,
+      items
+    };
+  };
 
   // retorna tudo que se tem E limpa o cart 
-  checkout() {};
+  checkout() {
+   const { total, items } = this.summary();
+ 
+    this.items = []
+
+    return {
+      total,
+      items
+    };
+  };
 
 }
--- p8-cart/src/Cart.spec.js
+++ p8-cart/src/Cart.spec.js
@@ -1,48 +1,122 @@
-import Cart from './Cart';
+import Cart from "./Cart";
 
-describe('check the Cart class operations', () => {
+
+describe("check the Cart class operations", () => {
   let cart;
   let product = {
-    title: 'Adidas runnign shoes - men',
-    price: 35388
+    title: "Adidas runnign shoes - men",
+    price: 35388,
+  };
+
+  let product2 = {
+    title: "Adidas runnign shoes - women",
+    price: 41872,
   };
 
   beforeEach(() => {
     cart = new Cart();
-  });  
-
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    const cart = new Cart();
-
-    expect(cart.getTotal()).toBe(0);
   });
 
-  it('should multiply quantity and price and receive the total amount', () => {
+  describe("getTotal()", () => {
+    it("should return 0 when getTotal() is executed in a newly created instance", () => {
+      const cart = new Cart();
 
-    const item = {
-      product,
-      quantity: 2
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toBe(70776);
-
-  });
-
-  it('should ensure no more than one product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2
+      expect(cart.getTotal()).toBe(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1
-    })
+    it("should multiply quantity and price and receive the total amount", () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
 
-    expect(cart.getTotal()).toEqual(70776);
-  })
+      cart.add(item);
 
+      expect(cart.getTotal()).toBe(70776);
+    });
 
+    it("should ensure no more than one product exists at a time", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it("should update total when a product gets included an then removed", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toBe(41872);
+    });
+  });
+
+  describe("checkout()", () => {
+    it("should return an object with the total and the list of items", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it("should reset the cart object when checkout() is called", () => {
+      cart.add({
+        product,
+        quantity: 3,
+      });
+
+      cart.checkout();
+    });
+  });
+
+  describe("summary()", () => {
+    it("should return the cart array with the added item", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+    });
+  });
+
+  describe('special conditions', () => {
+
+    it('should apply percentage discount above minimun is passed', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 2
+      });
+
+      expect(cart.getTotal()).toBe(70776);
+    });
+  });
 });
